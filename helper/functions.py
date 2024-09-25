b9cfb3cfa739d225ac046524cafbd914df91a7d9
Grygoriy Zolotarov <zolotaryovgl@gmail.com>
2024-09-25 14:05:12 +0200

    fixed possvm call

--- helper/functions.py
+++ helper/functions.py
@@ -38,15 +38,19 @@ def align_and_trim(input_file,output_file,ncpu = 1,mafft_opt = "",clipkit_mode =
         logging.info(cmd)
         subprocess.run(cmd, shell=True, check=True)
 
-def phylogeny(fasta_file, output_prefix, cptime = 1000, nstop = 100, nm = 10000, ntmax = 15, bb = 1000, quiet = ""):
+def phylogeny(fasta_file, output_prefix, cptime = 1000, nstop = 100, nm = 10000, ntmax = 15, bb = 1000, quiet = "",iqtree2 = "iqtree2"):
     logging.info(f"Phylogeny: {fasta_file} {output_prefix}")
-    cmd = f"iqtree2 -s {fasta_file} -m TEST -mset LG,WAG,JTT -nt AUTO -ntmax {ntmax} -bb {bb} -pre {output_prefix} -nm {nm} -nstop {nstop} -cptime {cptime} {quiet} --redo"
+    cmd = f"{iqtree2} -s {fasta_file} -m TEST -mset LG,WAG,JTT -nt AUTO -ntmax {ntmax} -bb {bb} -pre {output_prefix} -nm {nm} -nstop {nstop} -cptime {cptime} {quiet} --redo"
     logging.info(cmd)
     subprocess.run(cmd, shell=True, check=True)
 
 
 def possvm(treefile,output_prefix = None,reference_names = None, possvm = 'submodules/possvm-orthology/possvm.py'):
     logging.info(f"Possvm: {treefile}")
+    # get the location of the possvm submodule 
+    scriptdir = os.path.dirname(os.path.abspath(__file__))
+    possvm = scriptdir + '/../' + possvm
+    
     if reference_names:
         reference_names = f"-r {reference_names}"
     else:
